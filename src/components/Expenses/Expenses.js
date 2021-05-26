ef5659d9b11628a37156145aaf3a55b6011a0882
Manojlovic1998 <manojlovic911@hotmail.com>
2021-05-26 16:42:04 +0200

    Render list of data using .map to return JSX components for each item

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,28 +14,10 @@ function Expenses(props) {
 
     return (
         <div>
-            <ExpensesFilter selected={year} onChangeFilterYear={updateFilterYear}/>
             <Card className="expenses">
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date}
-                />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date}
-                />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date}
-                />
-                <ExpenseItem
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date}
-                />
+                <ExpensesFilter selected={year} onChangeFilterYear={updateFilterYear}/>
+                {props.items.map((expense) => (
+                    <ExpenseItem date={expense.date} amount={expense.amount} title={expense.title}/>))}
             </Card>
         </div>
     );
