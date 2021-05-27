036473aa5c34f6fa4c0d8ba14f1db1ca987dc859
Manojlovic1998 <manojlovic911@hotmail.com>
2021-05-27 02:14:03 +0200

    Add conditional logic for outputing content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,16 +15,21 @@ function Expenses(props) {
             return item.date.toLocaleString('en-eu', {year: 'numeric'}) === year;
     });
 
+    let expensesContent = <p>No expenses found.</p>
+    if (filterExpenseItems.length > 0) {
+        expensesContent = filterExpenseItems.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                date={expense.date}
+                amount={expense.amount}
+                title={expense.title}/>))
+    }
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={year} onChangeFilterYear={updateFilterYear}/>
-                {filterExpenseItems.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        date={expense.date}
-                        amount={expense.amount}
-                        title={expense.title}/>))}
+                {expensesContent}
             </Card>
         </div>
     );
